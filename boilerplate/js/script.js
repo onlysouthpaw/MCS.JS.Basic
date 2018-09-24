cfc983db9395216c778a90aa741c26fb5e7b5373
onlysouthpaw <43534436+onlysouthpaw@users.noreply.github.com>
2018-09-24 17:11:15 +0300

    how

--- boilerplate/js/script.js
+++ boilerplate/js/script.js
@@ -1,1 +1,9 @@
-console.log('You are at'+window.location);
+let o=+prompt('Сколько у вас с собой есть денег?')
+let y=+prompt('Сколько вы купили яблок?')
+let m=+prompt ('Сколько вы купили батонов хлеба?')
+let d=+prompt('Сколько стоит одно яблоко?')
+let g=y*d
+let h=+prompt('Сколько стоит один батон хлеба?')
+let k=m*h
+let l=o >=(h+k)
+document.body.innerHTML=l

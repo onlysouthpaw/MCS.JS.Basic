1f05fe488f26ea41d316edb12cb6af860d96241a
onlysouthpaw <43534436+onlysouthpaw@users.noreply.github.com>
2018-09-30 13:53:12 +0300

    ternary

---
+++ ternarytest/js2/script.js
@@ -0,0 +1,7 @@
+console.log('You are at '+window.location);
+
+let answer = prompt ('Введите число');
+answer = parseInt (answer);
+
+(answer>=0) ? console.log (Math.abs(answer)) : console.log (Math.abs(answer));
+

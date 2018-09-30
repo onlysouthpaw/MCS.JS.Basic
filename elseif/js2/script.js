57634ea8fe85bbe0b8b5b0328b00479dbeacb266
onlysouthpaw <43534436+onlysouthpaw@users.noreply.github.com>
2018-09-30 13:12:34 +0300

    elseif

---
+++ elseif/js2/script.js
@@ -0,0 +1,10 @@
+console.log('You are at '+window.location);
+
+let answer = prompt ('Введите число');
+answer = parseInt (answer);
+
+if (answer>=0){
+	console.log (Math.abs(answer));
+}else{
+	console.log (Math.abs(answer));
+}
